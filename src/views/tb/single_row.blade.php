4bde979ff5aaa2199e27d49c0e08bdd4b219f93a
Yaro <12fcv4@gmail.com>
2014-12-23 12:52:43 +0200

    table builder colored fields

--- src/views/tb/single_row.blade.php
+++ src/views/tb/single_row.blade.php
@@ -1,3 +1,13 @@
+
+<?php
+$trColor = '';
+foreach ($def['fields'] as $ident => $field) {
+    if (!$trColor && !$controller->getField($ident)->getAttribute('hide_list')) {
+        $trColor = $controller->getField($ident)->getRowColor($row);
+    }
+}
+?>
+
 <tr id-row="{{ $row['id'] }}">
     @if (isset($def['multi_actions']))
         <td><label class="checkbox multi-checkbox"><input type="checkbox" value="{{$row['id']}}" name="multi_ids[]" /><i></i></label></td>
@@ -5,11 +15,11 @@
     
     @foreach ($def['fields'] as $ident => $field)
     @if (!$controller->getField($ident)->getAttribute('hide_list'))
-
-    <td width="{{ $field['width'] or '' }}" class="{{ $field['class'] or '' }} unselectable">
-        @if ($controller->getField($ident)->getAttribute('fast-edit'))
-            <span class="dblclick-edit selectable">{{ $controller->getField($ident)->getListValue($row) }}</span>
-            {{ $controller->getField($ident)->getEditInput($row) }}
+    <?php $field = $controller->getField($ident) ?>
+    <td @if ($trColor) style="background-color: {{ $trColor }}" @endif width="{{ $field->getAttribute('width') }}" class="{{ $field->getAttribute('class') }} unselectable">
+        @if ($field->getAttribute('fast-edit'))
+            <span class="dblclick-edit selectable">{{ $field->getListValue($row) }}</span>
+            {{ $field->getEditInput($row) }}
             <div class="fast-edit-buttons">
                 <button class="btn btn-default btn-mini btn-save" type="button"
                         onclick="TableBuilder.saveFastEdit(this, {{ $row['id'] }}, '{{ $ident }}');">
@@ -20,7 +30,7 @@
                    onclick="TableBuilder.closeFastEdit(this, 'cancel');"></i>
             </div>
         @else
-            <span>{{ $controller->getField($ident)->getListValue($row) }}</span>
+            <span>{{ $field->getListValue($row) }}</span>
         @endif
     </td>
 
@@ -30,3 +40,5 @@
     {{ $controller->view->fetchActions($row) }}
     
 </tr>
+
+<?php  $trColor = ''; ?>

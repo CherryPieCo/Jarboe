3a10a20202d1aa658b7460648f1223cb14928163
Yaro <12fcv4@gmail.com>
2015-02-03 22:05:15 +0200

    remove trash

--- src/views/tree/tree_controls.blade.php
+++
@@ -1,5 +0,0 @@
-    <div>
-        <button type="button" class="pull-right btn btn-xs btn-default expanded" onclick="Tree.toggleCollapse(this);">
-            <i class="fa fa-caret-square-o-down"></i>
-        </button>
-    </div>

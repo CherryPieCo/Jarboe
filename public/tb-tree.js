59434bfdbed3939e8951e9ae586d38c9f8dee605
Yaro <12fcv4@gmail.com>
2015-02-17 09:20:19 +0200

    Update tb-tree.js

--- public/tb-tree.js
+++ public/tb-tree.js
@@ -3,14 +3,32 @@
 var Tree = 
 {
     admin_prefix: '',
-    parent_id: 99999,
+    parent_id: 1,
+    node: 1,
+    
+    setdbl: function() {
+        return false;
+        $(".jstree-anchor").on('dblclick', function(){ 
+            $(".jstree-anchor").unbind('dblclick');
+            Tree.setdbl();
+            console.log(this);
+            window.location.href = window.location.origin + window.location.pathname + '?node='+ $(this).parent().data('id');
+        });
+    }, // end 
     
     init: function()
     {
-        $('#tb-tree').jstree({
+        Tree.initModalCallbacks();
+        
+        
+        $('#tb-tree').on('after_open.jstree', function (e, data) {
+            Tree.setdbl();
+        }).jstree({
+            open_parents: true,
             "core" : {
-              //"check_callback" : true,
-              'check_callback': function(operation, node, node_parent, node_position, more) {
+                expand_selected_onload: true,
+                dblclick_toggle: true,
+                check_callback: function(operation, node, node_parent, node_position, more) {
                     // operation can be 'create_node', 'rename_node', 'delete_node', 'move_node' or 'copy_node'
                     // in case of 'rename_node' node_position is filled with the new node name
 
@@ -24,14 +42,56 @@ var Tree =
             "dnd": {
                 check_while_dragging: true
             },
-            "plugins" : [ "dnd" ]
-        }).bind("move_node.jstree", function (e, data) {
+            "contextmenu": {
+                "items": function ($node) {
+                    return {
+                        "Open": {
+                            "label": "Открыть",
+                            "action": function (obj) {
+                                window.location.href = window.location.origin + window.location.pathname + '?node='+ $(obj.reference[0].parentElement).data('id');
+                            },
+                            "separator_after" : true
+                        },
+                        "Create": {
+                            "label": "Добавить",
+                            "action": function (obj) {
+                                console.log($(obj.reference[0].parentElement).data('id'));
+                                Tree.showCreateForm($(obj.reference[0].parentElement).data('id'));
+                            }
+                        },
+                        "Rename": {
+                            "label": "Редактировать",
+                            "action": function (obj) {
+                                Tree.showEditForm($(obj.reference[0].parentElement).data('id'));
+                            }
+                        },
+                        "Delete": {
+                            "label": "Удалить",
+                            "icon" : "https://cdn1.iconfinder.com/data/icons/nuove/32x32/actions/fileclose.png",
+                            "action": function (obj) {
+                                var id = $(obj.reference[0].parentElement).data('id');
+                                if (id == 1) {
+                                    alert('Дропнуть полсайта? Ниет.');
+                                    return;
+                                }
+                                var $btn = $('.node-del-'+ id);
+                                Tree.doDelete(id, $btn);
+                                
+                                $(obj.reference[0].parentElement).remove();
+                            },
+                            "separator_before" : true
+                        }
+                    };
+                }
+            },
+            "plugins" : [ "dnd", "search", "contextmenu" ]
+        }).bind("move_node.jstree", function(e, data) {
            //console.log(data);
            //console.log($('#'+data.node.id).prev());
            
            var $current = jQuery('#'+data.node.id);
            jQuery.ajax({
-                url: Tree.admin_prefix +'/tb/tree/change-pos',
+                url: window.location.href,
                 type: 'POST',
                 dataType: 'json',
                 cache: false,
@@ -39,94 +99,224 @@ var Tree =
                     id:               $current.data('id'), 
                     parent_id:        jQuery('#'+data.parent).data('id'),
                     left_sibling_id:  $current.prev().data('id'), 
-                    right_sibling_id: $current.next().data('id')
+                    right_sibling_id: $current.next().data('id'),
+                    query_type:       'do_change_position'
                 },
                 success: function(response) {
                     if (response.status) {
                         $current.data('parent-id', response.parent_id);
+                        Tree.setdbl();
                     }
                 }
             });
+        }).bind("select_node.jstree", function (e, data) { 
+            //this binding lets
+            //us open all parents of the selected node
+            //console.log(data);
+            /*
+            if (typeof data.rslt !== 'undefined') {
+                data.rslt.obj.parents('.jstree-closed').each(function() {
+                    data.inst.open_node(this);
+                });
+            }
+            */
         });
-        
-        
-        //Tree.initNestable();
-        //Tree.initInnerNestable();
-        Tree.initResizableTable();
+
+        var to = false;
+        $('#plugins4_q').keyup(function () {
+            if(to) { clearTimeout(to); }
+            to = setTimeout(function () {
+                var v = $('#plugins4_q').val();
+                $('#tb-tree').jstree(true).search(v);
+            }, 250);
+        });
+        Tree.setdbl();
     }, // end saveMenuPreference
-    
-    initResizableTable: function()
+
+    activeToggle: function(id, isActive)
     {
-        $("#tb-tree-table").resizableColumns({
-            store: window.store
-        });
-    }, // end initResizableTable
-    
-    initNestable: function()
-    {
-        jQuery('#tb-tree').nestable({
-            maxDepth: 20,
-            onDragFinished  : function(currentNode, parentNode) {
-                jQuery.ajax({
-                    url: Tree.admin_prefix +'/tb/tree/change-pos',
-                    type: 'POST',
-                    dataType: 'json',
-                    cache: false,
-                    data: { 
-                        id:               jQuery(currentNode).data('id'), 
-                        parent_id:        jQuery(parentNode).data('id'),
-                        left_sibling_id:  jQuery(currentNode).prev().data('id'), 
-                        right_sibling_id: jQuery(currentNode).next().data('id')
-                    },
-                    success: function(response) {
-                        if (response.status) {
-                            jQuery(currentNode).data('parent-id', response.parent_id);
-                        }
-                    }
-                });
+        isActive = isActive ? 1 : 0;
+        jQuery.ajax({
+            url: window.location.href,
+            type: 'POST',
+            dataType: 'json',
+            cache: false,
+            data: { 
+                id: id,
+                is_active: isActive,
+                query_type: 'do_change_active_status'
+            },
+            success: function(response) {
             }
         });
-        //jQuery('#nestable3').nestable('collapseAll');
-    }, // end initNestable
+    }, // end activeToggle
+    
+    showCreateForm: function(id)
+    {
+        $('#cf-node', '#tree-create-modal').val(id);
+        $('#tree-create-modal').modal('show');
+    }, // end showCreateForm
+    
+    initModalCallbacks: function()
+    {
+        $('#tree-create-modal').on('hidden.bs.modal', function() {
+            $('#cf-node').val('');
+            $("#tree-create-modal-form")[0].reset();
+        });
+    }, // end initModalCallbacks
+    
+    doCreateNode: function()
+    {
+        var data = $('#tree-create-modal-form').serializeArray();
+        data.push({ name: 'query_type', value: 'do_create_node' });
         
-    initInnerNestable: function()
-    {
-        jQuery('#inner-nested').nestable({
-            maxDepth: 0,
-            onDragFinished  : function(currentNode, parentNode) {
-                jQuery.ajax({
-                    url: Tree.admin_prefix +'/tb/tree/change-pos',
-                    type: 'POST',
-                    dataType: 'json',
-                    cache: false,
-                    data: { 
-                        id:               jQuery(currentNode).data('id'), 
-                        parent_id:        jQuery(parentNode).data('id') ? jQuery(parentNode).data('id') : Tree.parent_id,
-                        left_sibling_id:  jQuery(currentNode).prev().data('id'), 
-                        right_sibling_id: jQuery(currentNode).next().data('id')
-                    },
-                    success: function(response) {
-                        if (response.status) {
-                            jQuery(currentNode).data('parent-id', response.parent_id);
-                        }
-                    }
-                });
+        jQuery.ajax({
+            url: window.location.href,
+            type: 'POST',
+            dataType: 'json',
+            cache: false,
+            data: data,
+            success: function(response) {
+                if (response.status) {
+                    // FIXME:
+                    window.location.reload();
+                } else {
+                    TableBuilder.showErrorNotification('Что-то пошло не так');
+                }
             }
         });
-        //jQuery('#nestable3').nestable('collapseAll');
-    }, // end initInnerNestable
+    }, // end doCreateNode
     
-    
-    toggleCollapse: function(context)
+    showEditForm: function(id)
     {
-        if ($(context).hasClass('expanded')) {
-            $('#tb-tree').nestable('collapseAll');
-            $(context).removeClass('expanded');
-        } else {
-            $('#tb-tree').nestable('expandAll');
-            $(context).addClass('expanded');
-        }
-    }, // end 
+        TableBuilder.showPreloader();
+        
+        jQuery.ajax({
+            url: window.location.href,
+            type: 'POST',
+            dataType: 'json',
+            cache: false,
+            data: { id: id, query_type: 'get_edit_modal_form' },
+            success: function(response) {
+                if (response.status) {
+                    
+                    console.log(response);
+                    
+                    jQuery(TableBuilder.form_wrapper).html(response.html);
+
+
+                    jQuery(TableBuilder.form_edit).modal('show');
+                    jQuery(TableBuilder.form_edit).find('input[data-mask]').each(function() {
+                        var $input = jQuery(this);
+                        $input.mask($input.attr('data-mask'));
+                    });
+
+
+                    TableBuilder.initSingleImageEditable();
+                    TableBuilder.initMultipleImageEditable();
+                    TableBuilder.initSummernoteFullscreen();
+                    TableBuilder.initSelect2Hider();
+                    
+                } else {
+                    TableBuilder.showErrorNotification('Что-то пошло не так');
+                }
+                
+                TableBuilder.hidePreloader();
+            }
+        });
+    }, // end showEditForm
+    
+    doEdit: function(id)
+    {
+        TableBuilder.showPreloader();
+        TableBuilder.showFormPreloader(TableBuilder.form_edit);
+
+        var values = jQuery(TableBuilder.edit_form).serializeArray();
+        values.push({ name: 'id', value: id });
+        values.push({ name: 'query_type', value: "do_edit_node" });
+
+        // take values from temp storage (for images)
+        jQuery.each(values, function(index, val) {
+            if (typeof TableBuilder.storage[val.name] !== 'undefined') {
+                var json = JSON.stringify(TableBuilder.storage[val.name]);
+                values[index] = {
+                    name:  val.name,
+                    value: json
+                };
+            }
+        });
+
+        /* Because serializeArray() ignores unset checkboxes and radio buttons: */
+        values = values.concat(
+            jQuery(TableBuilder.edit_form).find('input[type=checkbox]:not(:checked)')
+                .map(function() {
+                    return { "name": this.name, "value": 0 };
+                }).get()
+        );
+        console.log(values);
+        
+        jQuery.ajax({
+            type: "POST",
+            url: window.location.href,
+            data: values,
+            dataType: 'json',
+            success: function(response) {
+
+                TableBuilder.hideFormPreloader(TableBuilder.form_edit);
+
+                if (response.id) {
+                    TableBuilder.showSuccessNotification("Поле обновлено успешно");
+                    
+                    jQuery(TableBuilder.form_edit).modal('hide');
+
+                    jQuery('.tb-tree-content-inner').find('tr[data-id="'+ id +'"]').replaceWith(response.html);
+
+                } else {
+                    var errors = '';
+                    jQuery(response.errors).each(function(key, val) {
+                        errors += val +'<br>';
+                    });
+                    TableBuilder.showBigErrorNotification(errors);
+                }
+
+                TableBuilder.hidePreloader();
+            }
+        });
+    }, // end doEdit
+    
+    doDelete: function(id, context)
+    {
+        jQuery.SmartMessageBox({
+            title : "Удалить?",
+            content : "Эту операцию нельзя будет отменить.",
+            buttons : '[Нет][Да]'
+        }, function(ButtonPressed) {
+            if (ButtonPressed === "Да") {
+                TableBuilder.showPreloader();
+
+                jQuery.ajax({
+                    type: "POST",
+                    url: window.location.href,
+                    data: { id: id, query_type: 'do_delete_node' },
+                    dataType: 'json',
+                    success: function(response) {
+
+                        if (response.status) {
+                            TableBuilder.showSuccessNotification('Поле удалено успешно');
+
+                            jQuery(context).parent().parent().remove();
+                        } else {
+                            TableBuilder.showErrorNotification('Что-то пошло не так, попробуйте позже');
+                        }
+
+                        TableBuilder.hidePreloader();
+                    }
+                });
+
+            }
+
+        });
+    }, // end doDelete
     
 };
 

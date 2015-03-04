889d7fb4f9600c046b21f38b278f87cd844115c7
Yaro <12fcv4@gmail.com>
2015-03-04 15:36:17 +0200

    add with_link attribute for checkboxes m2m field

--- src/views/tb/input_many2many_checkbox.blade.php
+++ src/views/tb/input_many2many_checkbox.blade.php
@@ -5,14 +5,28 @@ $colNum = 12 / $divide;
     @foreach ($options as $option)
     <div class="col col-{{$colNum}}">
         @foreach ($option as $key => $title)
-        <label class="checkbox">
-            <input type="checkbox" 
-                   name="{{$name}}[{{$key}}]" 
-                   @if (isset($selected[$key]))
-                       checked="checked"
-                   @endif
-                   >
-            <i></i>{{$title}}</label>
+            @if ($link)
+            <label class="checkbox" style="float:left;">
+                <input type="checkbox" 
+                       name="{{$name}}[{{$key}}]" 
+                       @if (isset($selected[$key]))
+                           checked="checked"
+                       @endif
+                       >
+                <i></i></label>
+                <span style="line-height: 25px;">
+                    <a href="{{ url($link .'?edit='. $key) }}" target="_blank">{{{$title}}}</a>
+                </span>
+            @else
+            <label class="checkbox">
+                <input type="checkbox" 
+                       name="{{$name}}[{{$key}}]" 
+                       @if (isset($selected[$key]))
+                           checked="checked"
+                       @endif
+                       >
+                <i></i>{{{$title}}}</label>
+            @endif
         @endforeach
     </div>
     @endforeach

7fc33e6836abee2896cca0f511ab4b4aaff431cb
Yaro <12fcv4@gmail.com>
2015-03-18 11:56:42 +0200

    add back buttons to row edit page

--- src/views/table_page.blade.php
+++ src/views/table_page.blade.php
@@ -1,20 +1,57 @@
 @extends('admin::layouts.default')
 
 
+@section('headline')
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom: 20px;">
+<a href="javascript:void(0);" onclick="window.history.back();" class="btn btn-default">Назад</a>
+<a href="{{ url($definition['db']['pagination']['uri']) }}" class="btn btn-info">К таблице</a>
+</div>
+</div>
+@stop
+
+
+
 @section('main')
 
+<section>
 <div class="row">
-    <div class="table-page">
-    {{ $form }}
-    
-    {{ $js }}
-    </div>
+    <article class="col-sm-12 ">
+        <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-x22" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-sortable="false" role="widget" style="">
+            <header role="heading">
+                <span class="widget-icon"> <i class="fa fa-align-justify"></i> </span>
+                <h2>Редактирование #{{{$id}}} </h2>
+            <span class="jarviswidget-loader"><i class="fa fa-refresh fa-spin"></i></span></header>
+            <!-- widget div-->
+            <div role="content">
+                <!-- widget edit box -->
+                <div class="jarviswidget-editbox">
+                </div>
+                <!-- end widget edit box -->
+
+                <!-- widget content -->
+                <div class="widget-body">
+                    <div class="table-page">
+                    {{ $form }}
+                    
+                    {{ $js }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
 </div>
+</section>
+
+
+
 
 <style>
-.table-page .tab-content,
+.table-page {
+    margin: -25px;
+}
 .table-page .modal-footer {
-    background: rgba(255,255,255,.9);
+
 }
 </style>
 

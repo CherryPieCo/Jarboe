93efd9a62d70b5a316eb53371926510459dd117e
Yaro <12fcv4@gmail.com>
2015-03-24 11:00:09 +0200

    add flush filter input button

--- src/views/tb/filter_text.blade.php
+++ src/views/tb/filter_text.blade.php
@@ -1,1 +1,9 @@
-<input type="text" value="{{ $value }}" name="filter[{{ $name }}]" class="form-control input-small" />
+
+<div style="position: relative;">
+    <input type="text" value="{{ $value }}" name="filter[{{ $name }}]" class="form-control input-small" />
+    @if ($value)
+    <button onclick="$(this).parent().find('input').val(''); setTimeout(function(){ TableBuilder.search(); }, 200);" class="close" style="position: absolute; top: 8px; right: 6px;">
+        ×
+    </button>
+    @endif
+</div>

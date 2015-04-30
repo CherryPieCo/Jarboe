752e66c5a42520dd26b99f515bfc4d03c0d92757
Yaro <12fcv4@gmail.com>
2015-04-30 12:19:40 +0300

    according to psr-2

--- src/Yaro/Jarboe/Helpers/Settings.php
+++ src/Yaro/Jarboe/Helpers/Settings.php
@@ -18,7 +18,7 @@ class Settings
     {
     } // end __clone
 
-    static public function getInstance()
+    public static function getInstance()
     {
         if (is_null(self::$instance)) {
             self::$instance = new self();

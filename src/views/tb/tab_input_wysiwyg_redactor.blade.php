23dcca25e48c0b2e03b2f3a4e68fbd60c6b8e36a
Yaro <12fcv4@gmail.com>
2015-05-06 16:40:07 +0300

    image info to data-* attrs

--- src/views/tb/tab_input_wysiwyg_redactor.blade.php
+++ src/views/tb/tab_input_wysiwyg_redactor.blade.php
@@ -39,7 +39,7 @@
                                     //TableBuilder.uploadImageFromWysiwygSummertime(files, editor, $editable);
                                 },
                                 <?php // FIXME: ?>
-                                imageManagerJson: '{{ url($action) }}?query_type=image_storage&storage_type=get_redactor_images_list',
+                                imageManagerJson: '{{ url($action) }}?query_type=image_storage&storage_type=get_redactor_images_list&__node={{ \Input::get('__node') }}',
                                 plugins: ['imagemanager']
                             });
                         });

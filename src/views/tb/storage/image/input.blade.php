92b9d81d60443805a4cf5a8ccdf1c129374aff1f
Yaro <12fcv4@gmail.com>
2015-05-14 09:35:23 +0300

    fix nonvalue preview from storage

--- src/views/tb/storage/image/input.blade.php
+++ src/views/tb/storage/image/input.blade.php
@@ -15,8 +15,8 @@
         <a onclick="TableBuilder.openImageStorageModal(this, '{{$type}}');" style="width:100%;" class="btn btn-info btn-sm" href="javascript:void(0);">Открыть</a>
     </div>
     
-    @if ($row)
-        <img style="height: 90px;" src="{{ glide($entity->source, []) }}">
+    @if ($row && $entity)
+        <img style="height: 90px;" src="{{ glide($entity->source, ['h' => 90]) }}">
     @endif
 </div>
 

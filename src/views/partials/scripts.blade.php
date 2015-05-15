d162e1365ba2ab9e496a2dbfe7696e54cc673762
Yaro <12fcv4@gmail.com>
2015-05-15 12:19:52 +0300

    fix 401 response

--- src/views/partials/scripts.blade.php
+++ src/views/partials/scripts.blade.php
@@ -17,12 +17,11 @@
 
 <script>
     $(document).ajaxComplete(function(event, xhr, settings) {
-        var response = $.parseJSON(xhr.responseText);
-        console.log(response);
         if (xhr.status == 401) {
             $('#locked-screen').show();
             $('.tb-modal:visible').addClass('superbox-modal-hide').hide();
         } else if (xhr.status == 500) {
+            var response = $.parseJSON(xhr.responseText);
             $.bigBox({
                 title : response.error.type,
                 content : '<p style="overflow-y: auto; word-break: break-all; height: 100px;">'+ response.error.message +"<br>@"+ response.error.line +"<br>"+ response.error.file +'</p>',

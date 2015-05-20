6d43b11c390e197cb2150f2a81d3c1cfb23d83c3
Yaro <12fcv4@gmail.com>
2015-05-20 10:17:41 +0300

    add title for content top header

--- src/views/tree/content_header.blade.php
+++ src/views/tree/content_header.blade.php
@@ -10,3 +10,15 @@
         <a href="?node={{ $ancestor->id }}">{{ $ancestor->slug }}</a> / 
     @endif
 @endforeach
+
+<hr style="margin: 3px 0;">
+
+@foreach ($ancestors as $ancestor)
+    @if ($ancestor->slug == '/' || !$ancestor->slug)
+        <a href="?node=1">{{ $ancestor->title }}</a> / 
+    @elseif ($loop->last)
+        {{ $ancestor->title }}
+    @else
+        <a href="?node={{ $ancestor->id }}">{{ $ancestor->title }}</a> / 
+    @endif
+@endforeach

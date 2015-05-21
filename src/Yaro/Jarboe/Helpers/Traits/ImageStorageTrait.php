10244fe0e7e5dbfce2f1e403dbd7eb187ef5fd5e
Yaro <12fcv4@gmail.com>
2015-05-21 16:28:02 +0300

    trait for image storage

--- src/Yaro/Jarboe/Helpers/Traits/ImageStorageTrait.php
+++ src/Yaro/Jarboe/Helpers/Traits/ImageStorageTrait.php
@@ -6,11 +6,41 @@ namespace Yaro\Jarboe\Helpers\Traits;
 trait ImageStorageTrait
 {
 
-    public function getStorageImage($field = 'image')
+    public function doImagesParse($value)
     {
         $model = \Config::get('jarboe::images.models.image');
+        $images = \Cache::tags('images', 'jarboe')->get('images', function() use($model) {
+            return $model::all(); 
+        });
         
-        return $model::find(trim($this->$field));
-    } // end getStorageImage
+        preg_match_all('~<img[^>]+data-j_images_id="(\d+)"[^>]*>~', $value, $matches);
+        list($html, $ids) = $matches;
+        
+        $patterns = array();
+        $replacements = array();
+        foreach ($ids as $index => $id) {
+            $image = $food->filter(function($item) use($id) {
+                return $item->id == $id;
+            })->first();
+            
+            $patterns[] = '~'. preg_quote($html[$index]) .'~';
+            $replacements[] = $image ? $this->fetchImage($image) : '';
+        }
+        preg_replace($patterns, $replacements, $value);
+        
+        return $value;
+    } // end doImagesParse
+    
+    public function fetchImage($image)
+    {
+        $html = '<img class="j-image"';
+        $info = json_encode($image->info, true) ? : array();
+        foreach ($info as $key => $val) {
+            $html .= ' data-'. $key .'="'. $val .'"';
+        }
+        $html .= '>';
+        
+        return $html;
+    } // end fetchImage
 
 }

c31093111e0b918257b28509e707966026a48716
Yaro <12fcv4@gmail.com>
2015-06-03 10:33:38 +0300

    image storage trait: save attributes and styles for image

--- src/Yaro/Jarboe/Helpers/Traits/ImageStorageTrait.php
+++ src/Yaro/Jarboe/Helpers/Traits/ImageStorageTrait.php
@@ -24,23 +24,33 @@ trait ImageStorageTrait
             })->first();
 
             $patterns[] = '~'. preg_quote($html[$index]) .'~';
-            $replacements[] = $image ? $this->fetchImage($image) : '';
+            $replacements[] = $image ? $this->fetchImage($image, $html[$index]) : '';
         }
 
         return preg_replace($patterns, $replacements, $value);
     } // end doImagesParse
     
-    public function fetchImage($image)
+    public function fetchImage($image, $htmlOrigin = false)
     {
         $wysiwygSource = \Config::get('jarboe::images.wysiwyg_image_type');
         $html = '<img class="j-image" src="'. asset($image->$wysiwygSource) 
               . '" data-source="'. asset($image->source) .'"';
 
         $info = $image->info ? json_decode($image->info, true) : array();
-
         foreach ($info as $key => $val) {
             $html .= ' data-'. $key .'="'. $val .'"';
         }
+        
+        if ($htmlOrigin) {
+            preg_match_all('~((rel|style|data|id|onclick)=(\"|\'){1}([^\"\'])(\"|\'){1})~', $htmlOrigin, $matches);
+            if (isset($matches[0])) {
+                foreach ($matches as $attribute) {
+                    $html .= ' '. $attribute;
+                }
+            }
+        }
+            
+        
         $html .= '>';
 
         return $html;

dc485653559423e4c323c3627103d17c39d24a61
Yaro <12fcv4@gmail.com>
2015-06-05 14:02:24 +0300

    min enh

--- src/view_composers.php
+++ src/view_composers.php
@@ -19,7 +19,7 @@ View::composer('admin::tb.storage.image.images', function($view) {
     $perPage = Config::get('jarboe::images.per_page');
     
     $model = Config::get('jarboe::images.models.image');
-    $images = $model::orderBy('id', 'desc')->skip(0)->limit($perPage)->get();
+    $images = $model::orderBy('created_at', 'desc')->skip(0)->limit($perPage)->get();
     
     $view->with('images', $images)->with('fields', $fields);
 });

b604951595f04c70c8083693980bc07c98994aca
Yaro <12fcv4@gmail.com>
2015-06-12 12:54:40 +0300

    make wysiwyg imredactor for default

--- src/Yaro/Jarboe/Fields/WysiwygField.php
+++ src/Yaro/Jarboe/Fields/WysiwygField.php
@@ -32,7 +32,7 @@ class WysiwygField extends AbstractField
             }
         }
 
-        $wysiwyg = $this->getAttribute('wysiwyg', 'summernote');
+        $wysiwyg = $this->getAttribute('wysiwyg', 'redactor');
 
         $input = \View::make('admin::tb.input_wysiwyg' .'_'. $wysiwyg);
         $input->value = $this->getValue($row);
@@ -78,7 +78,7 @@ class WysiwygField extends AbstractField
             }
         }
         
-        $wysiwyg = $this->getAttribute('wysiwyg', 'summernote');
+        $wysiwyg = $this->getAttribute('wysiwyg', 'redactor');
         
         $input = \View::make('admin::tb.tab_input_wysiwyg' .'_'. $wysiwyg);
         $input->value = $this->getValue($row);
